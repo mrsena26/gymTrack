6f06e836b386ebf8b1af1ea38bf40893b3d02310
mrsena26 <mrsena26@hotmail.com>
2026-02-15 18:43:23 +0000

    Add end session logic and UI update to end session

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -2,11 +2,40 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Session, Exercise, Set
 from .serializers import SessionSerializer, ExerciseSerializer, SetSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all()
     serializer_class =SessionSerializer
+
+    def perform_create(self, serializer):
+        if Session.objects.filter(status="active").exists():
+            raise ValidationError("An active session already exists.")
+        serializer.save()
+    @action(detail=False, methods=["get"])
+    def active(self, request):
+        session = Session.objects.filter(status="active").first()
+
+        if session:
+            serializer = self.get_serializer(session)
+            return Response(serializer.data)
+        return Response({"message": "No active session"}, status=404)
+    @action(detail=True, methods=["patch"])
+    def end(self, request, pk=None):
+        session = self.get_object()
+
+        if session.status != "active":
+            return Response(
+                {"error": "Session is not active"},
+                status=400
+            )
+        session.status = "completed"
+        session.save()
+
+        return Response({"message": "Session ended"})
 
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
