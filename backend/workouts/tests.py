1afeb56362f3ff3dd14b58d831293939a3d4dfc1
mrsena26 <mrsena26@hotmail.com>
2026-02-15 15:05:03 +0000

    add only 1 active session validation and some tests

--- backend/workouts/tests.py
+++ backend/workouts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient
+from .models import Session
 # Create your tests here.
+
+class SessionTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_session_success(self):
+        response = self.client.post("/api/sessions/", {})
+        self.assertEqual(response.status_code,201)
+    
+    def test_cannot_create_two_active_sessions(self):
+        self.client.post("/api/sessions/",{})
+        response = self.client.post("/api/sessions/",{})
+        
+        self.assertEqual(response.status_code,400)
+        self.assertIn("active", str(response.data).lower())
