089ce33c1f5d29540d0ca1e702c3810abbb3b341
mrsena26 <mrsena26@hotmail.com>
2026-02-21 01:07:43 +0000

    add frontend UI to add exercise and sets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,61 +3,190 @@ import { useEffect, useState } from "react";
 function App() {
   const [activeSession, setActiveSession] = useState(null);
   const [message, setMessage] = useState("");
+  const [exerciseName, setExerciseName] = useState("");
+  const [muscleGroup, setMuscleGroup] = useState("");
+  const [setInputs, setSetInputs] = useState({});
 
   useEffect(() =>{
-    checkActiveSession();
+    fetchActiveSession();
   }, []);
 
-  const checkActiveSession = async() => {
+  const fetchActiveSession = async() => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/sessions/active/");
-      if (response.ok){
-        const data = await response.json();
-        setActiveSession(data);
-      }
-    } catch(error){
-      console.error("Error checking session", error);
-    }
+    const data = await apiRequest(
+      "http://127.0.0.1:8000/api/sessions/active/"
+    );
+    setActiveSession(data);
+  } catch (err) {
+    setActiveSession(null);
+  }
   
   };
 
   const startSession = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/sessions/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({})
-    });
-
-    if(response.ok){
-      const data = await response.json();
+    try {
+      const data = await apiRequest(
+        "http://127.0.0.1:8000/api/sessions/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+      }
+      );
       setActiveSession(data);
-    } else{
-      const error = await response.json();
-      setMessage(error.detail || "Error starting session");
+      setMessage("");
+    }catch(err){
+      setMessage(err.message);
     }
 
-    const data = await response.json();
-    setMessage(JSON.stringify(data));
   };
 
   const endSession = async () =>{
-    const response = await fetch(
+    try {
+    await apiRequest(
       `http://127.0.0.1:8000/api/sessions/${activeSession.id}/end/`,
-      { method: "PATCH" });
-      if (response.ok){
-        setActiveSession(null);
-      }
+      { method: "PATCH" }
+    );
+
+      setActiveSession(null);
+    } catch (err) {
+      setMessage(err.message);
+    }
   };
+  const addExercise = async() => {
+    try{
+       await apiRequest(
+      "http://127.0.0.1:8000/api/exercises/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          session: activeSession.id,
+          name: exerciseName,
+          muscle_group: muscleGroup
+        })
+      }
+    );
+    setExerciseName("");
+    setMuscleGroup("");
+    await fetchActiveSession();
+    }catch(err){
+      setMessage(err.message);
+    }
+  };
+
+  const addSet = async (exerciseId) => {
+    const reps = setInputs[exerciseId]?.reps;
+    const weight = setInputs[exerciseId]?.weight;
+
+    if(!reps || !weight)return;
+    try {
+      await apiRequest(
+      "http://127.0.0.1:8000/api/sets/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          exercise: exerciseId,
+          reps: reps,
+          weight: weight
+        })
+      }
+    );
+
+    setSetInputs(prev => ({
+      ...prev,
+      [exerciseId]: { reps: "", weight: "" }
+    }));
+
+    await fetchActiveSession();
+    } catch(err){
+      setMessage(err.message);
+    }
+};
+
+const apiRequest = async (url, options = {}) => {
+  const response = await fetch(url, options);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.detail || data.error || "Something went wrong");
+  }
+
+  return data;
+};
 
   return (
     <div>
       <h1>Gym Tracker</h1>
       {activeSession ? (
-        <div>
+        <div style={{marginBottom: "20px"}}>
           <p>Session ongoing (ID: {activeSession.id})</p>
           <button onClick={endSession}>End Session</button>
+          <div style={{marginBottom: "20px"}}>
+             <h3>Add Exercise</h3>
+              <input
+                type="text"
+                placeholder="Exercise name"
+                value={exerciseName}
+                onChange={(e) => setExerciseName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Muscle group"
+                value={muscleGroup}
+                onChange={(e) => setMuscleGroup(e.target.value)}
+              />
+              <button onClick={addExercise}>
+                Add Exercise
+              </button>
+          </div>
+          {activeSession && activeSession.exercises.map((exercise) => (
+            <div key={exercise.id}>
+              <h3>{exercise.name} ({exercise.muscle_group})</h3>
+
+              {exercise.sets.map((set) => (
+                <div key={set.id}>
+                  {set.reps} reps @ {set.weight}
+                </div>
+              ))}
+
+              <input
+                type="number"
+                placeholder="Reps"
+                value={setInputs[exercise.id]?.reps || ""}
+                onChange={(e) =>
+                setSetInputs(prev => ({
+                  ...prev,
+                  [exercise.id]: {
+                    ...prev[exercise.id],
+                    reps: e.target.value
+                    }
+                  }))
+                }
+              />
+
+              <input
+                type="number"
+                placeholder="Weight"
+                value={setInputs[exercise.id]?.weight || ""}
+                onChange={(e) =>
+                  setSetInputs(prev => ({
+                    ...prev,
+                    [exercise.id]: {
+                      ...prev[exercise.id],
+                      weight: e.target.value
+                    }
+                  }))
+                }
+              />
+
+              <button onClick={() => addSet(exercise.id)}>
+                Add Set
+              </button>
+            </div>
+          ))}
         </div>
       ): (
         <button onClick={startSession}>Start Session</button>
